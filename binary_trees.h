ee4e824e17b6f197c98ac4c4d7e358d31c4d211e
Franc6220 <samuelwangalibo@yahoo.com>
2024-06-08 10:34:43 -0700

    header

--- binary_trees.h
+++ binary_trees.h
@@ -1,7 +1,11 @@
 #ifndef BINARY_TREES_H
 #define BINARY_TREES_H
 
+#include <stddef.h>
+#include <stdio.h>
 #include <stdlib.h>
+
+/* Basic Binary Tree */
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -21,7 +25,18 @@ struct binary_tree_s
 
 typedef struct binary_tree_s binary_tree_t;
 
-/* Function prototypes */
+/* Binary Search Tree */
+typedef struct binary_tree_s bst_t;
+
+/* AVL Tree */
+typedef struct binary_tree_s avl_t;
+
+/* Max Binary Heap */
+typedef struct binary_tree_s heap_t;
+
+/* Prototypes */
+
+/* Task 0 - 14 */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
@@ -46,21 +61,39 @@ void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 int binary_tree_is_complete(const binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
+
+/* Task 24 - 27 */
 int binary_tree_is_bst(const binary_tree_t *tree);
 bst_t *bst_insert(bst_t **tree, int value);
 bst_t *array_to_bst(int *array, size_t size);
 bst_t *bst_search(const bst_t *tree, int value);
 bst_t *bst_remove(bst_t *root, int value);
+
+/* Task 28 */
 int binary_tree_is_avl(const binary_tree_t *tree);
+
+/* Task 29 - 34 */
 avl_t *avl_insert(avl_t **tree, int value);
 avl_t *array_to_avl(int *array, size_t size);
 avl_t *avl_remove(avl_t *root, int value);
 avl_t *sorted_array_to_avl(int *array, size_t size);
+
+/* Task 35 - 38 */
 int binary_tree_is_heap(const binary_tree_t *tree);
 heap_t *heap_insert(heap_t **root, int value);
 heap_t *array_to_heap(int *array, size_t size);
 int heap_extract(heap_t **root);
 int *heap_to_sorted_array(heap_t *heap, size_t *size);
+
+/* Task 39 - 41 */
+int heap_extract(heap_t **root);
+int *heap_to_sorted_array(heap_t *heap, size_t *size);
+#define BINARY_HEAP_INSERT_COMPLEXITY O(log n)
+#define BINARY_HEAP_EXTRACT_COMPLEXITY O(log n)
+#define BINARY_HEAP_SEARCH_COMPLEXITY O(n)
+
+/* Utilities */
+void binary_tree_print(const binary_tree_t *tree);
 
 #endif /* BINARY_TREES_H */
 
